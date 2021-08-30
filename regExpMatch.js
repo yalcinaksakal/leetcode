9715372f254dea4086d92f25ffc53cf305e92311
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-08-30 02:46:17 +0200

    10

--- regExpMatch.js
+++ regExpMatch.js
@@ -1,3 +1,5 @@
+// 10. Regular Expression Matching
+
 // Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
 
 // '.' Matches any single character.​​​​
@@ -29,52 +31,93 @@
 // Input: s = "mississippi", p = "mis*is*p*."
 // Output: false
 
-const isMacth = (s, p) => {
-  if (!p) return !s;
-  indexS = 0;
+const isMatch = function (s, p) {
+  // if (!p) return true;
+
   x = p.split("*").length - 1;
   if (s.length < p.length - 2 * x) return false;
+
+  indexS = 0;
   for (i = 0; i < p.length; i++) {
     if (p[i] === "*") continue;
     if (p[i + 1] === "*") {
-      while ((p[i] === s[indexS] || p[i] === ".") && indexS < s.length)
-        indexS++;
-
-      continue;
+      k = indexS;
+      check = false;
+      pattern = p.slice(i + 2);
+      repeatingChar = p[i];
+      while (
+        s[k] &&
+        !check &&
+        (s[k] === repeatingChar || repeatingChar === "." || !(k - indexS))
+      ) {
+        j = 0;
+        while (j < s.length && !check) {
+          check = isMatch(s.slice(k), repeatingChar.repeat(j) + pattern);
+          j++;
+        }
+        k++;
+      }
+      return check;
     }
+
     if (p[i] === s[indexS] || (p[i] === "." && s[indexS])) {
       indexS++;
-      continue;
-    }
-    if (p[i] !== s[indexS] && p[i] !== "." && s[indexS]) {
-      return false;
-    }
-
-    if (p[i - 1] === "*") {
-      indexS -= p.length - i;
-      i--;
       continue;
     }
     return false;
   }
   return indexS >= s.length;
 };
-result = isMacth("a", "ab*a");
-console.log(result);
 
-// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+const isMatch2 = function (s, p) {
+  if (!p || !s) return true;
 
-var maxArea = function (height) {
-  maxArea = 0;
-  left = 0;
-  right = height.length - 1;
-  while (left < right) {
-    maxArea = Math.max(
-      maxArea,
-      Math.min(height[left], height[right]) * (right - left)
-    );
-    if (height[left] < height[right]) left++;
-    else right--;
+  x = p.split("*").length - 1;
+  if (s.length < p.length - 2 * x) return false;
+
+  indexS = 0;
+  for (i = 0; i < p.length; i++) {
+    if (p[i] === "*") continue;
+
+    if (p[i + 1] === "*") {
+      if (p[i] === "." && !p[i + 2]) return true;
+
+      if (p[i] === "." && p[i + 2] !== ".") {
+        while (s[indexS] !== p[i + 2] && s[indexS]) indexS++;
+        if (!s[indexS]) return false;
+        continue;
+      }
+      j = i + 2;
+      while (p[i] === p[j]) j++;
+      count = j - i - 2;
+      //count times p[i-1] char should be in s
+      if (p[i + 2] === ".") {
+        while (s[indexS] === p[i]) indexS++;
+        continue;
+      }
+      countInS = 0;
+      while (s[indexS] === p[i]) {
+        indexS++;
+        countInS++;
+      }
+      if (countInS < count) return false;
+      continue;
+    }
+
+    if (p[i] === s[indexS] || (p[i] === "." && s[indexS])) {
+      indexS++;
+      continue;
+    }
   }
-  return maxArea;
+  return indexS >= s.length;
 };
+// console.log(isMatch("bbbba", ".*a*a"));
+// console.log(isMatch("ab", ".*c"));
+// console.log(isMatch("ab", ".*.."));
+// console.log(isMatch("mississippi", "mis*is*ip*."));
+//  console.log(isMatch("abcdedededecccc", "ab.*dec*"));
+// console.log(isMatch("ip", "s*p"));
+// console.log(isMatch("aa", "."));
+// console.log(isMatch("aa", "b*a"));
+// console.log(isMatch("ab", ".*"));
+console.log(isMatch("aa", "b*ac*a"));
