b1f92fbba5db6bebc7d1e5a283c45bdea13c15f1
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-09-02 01:01:01 +0200

    561

--- solvedLeetCode/7reverse.js
+++ solvedLeetCode/7reverse.js
@@ -1,5 +1,3 @@
-// 7. Reverse Integer
-
 // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
 // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
@@ -21,7 +19,22 @@
 // Input: x = 0
 // Output: 0
 
-const reverse = x => {
+var reverse = function (x) {
+  if (!x) return x;
+  sign = x < 0 ? -1 : 1;
+  x *= sign;
+  result = (x + "").split("").reverse().join("");
+  i = 0;
+  while (result[i] === "0") {
+    i++;
+  }
+  result = result.slice(i);
+  result = (+result | 0) == result ? result : 0;
+  return result * sign;
+};
+
+//better
+const reverse2 = x => {
   result = (x + "")
     .split("")
     .reverse()
@@ -31,4 +44,4 @@ const reverse = x => {
   // (+a | 0) == a;
   return (+result | 0) == result ? result : 0;
 };
-console.log(reverse(1222222222277999));
+console.log(reverse2(-1023000));
