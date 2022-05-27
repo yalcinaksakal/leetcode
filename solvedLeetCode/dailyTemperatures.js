93891b252f6d38ab673036005ed40d85370c7931
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-05-28 00:56:41 +0200

    move

--- solvedLeetCode/dailyTemperatures.js
+++ solvedLeetCode/dailyTemperatures.js
@@ -4,19 +4,16 @@
  */
 var dailyTemperatures = function (temperatures) {
 	const res = Array(temperatures.length).fill(0),
-		temp = [{ t: temperatures[0], i: 0 }];
+		temp = [];
 	let l, found;
 
-	for (let i = 1; i < temperatures.length; i++) {
+	for (let i = 0; i < temperatures.length; i++) {
 		l = temp.length - 1;
-		while (l > -1 && temp[l].t < temperatures[i]) {
+		while (l > -1 && temp[l--].t < temperatures[i]) {
 			found = temp.pop();
 			res[found.i] = i - found.i;
-			l--;
 		}
 		temp.push({ t: temperatures[i], i });
 	}
 	return res;
 };
-
-dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]);
