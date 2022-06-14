7627af4652128927dbe917dbd65e09aeef60cc07
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-06-14 22:01:06 +0200

    1337

---
+++ 1337.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = function (mat, k) {
+	const weakest = [],
+		n = mat[0].length - 1,
+		countSoldiers = row => {
+			let low = 0,
+				mid,
+				high = n;
+			while (low < high) {
+				mid = (low + high) >>> 1;
+				mat[row][mid] ? (low = mid + 1) : (high = mid);
+			}
+			return low + mat[row][low];
+		};
+	for (let i = 0; i < mat.length; i++) weakest.push([i, countSoldiers(i)]);
+	weakest.sort((a, b) => {
+		if (a[1] < b[1]) return -1;
+		if (a[1] === b[1] && a[0] < b[0]) return -1;
+		return 0;
+	});
+	return weakest.slice(0, k).map(r => r[0]);
+};
+
+kWeakestRows(
+	[
+		[1, 1, 1, 1, 1],
+		[1, 0, 0, 0, 0],
+		[1, 1, 0, 0, 0],
+		[1, 1, 1, 1, 0],
+		[1, 1, 1, 1, 1],
+	],
+	3
+);
