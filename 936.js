49ae35a8985929957536d614c573f9a41ef9b509
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-08-22 01:08:04 +0200

    936-2

--- 936.js
+++ 936.js
@@ -3,6 +3,39 @@
  * @param {string} target
  * @return {number[]}
  */
+
+/*
+      SOLUTION:
+      1- Try to find the last stamp position. Let stamp=abca, target=aabcacaaaaaaaabca
+      2- Find max exact matching position for stamp
+             abca
+            aabcacaaaaaaaabca
+      3- Mark this position as stamped
+            a????caaaaaaaabca    stamped positions=[1];
+      4- Repeat this process while we can find exact matching characters
+                         abca
+            a????caaaaaaa????    stamped positions=[13,1];
+               abca
+            a??????aaaaaa????    stamped positions=[3,13,1]; 
+            abca
+            ???????aaaaaa????    stamped positions=[0,3,13,1]; 
+                abca      
+            ????????aaaaa????    stamped positions=[4,0,3,13,1]; 
+                 abca
+            ?????????aaaa????    stamped positions=[5,4,0,3,13,1]; 
+                  abca
+            ??????????aaa????    stamped positions=[6,5,4,0,3,13,1]; 
+                   abca
+            ???????????aa????    stamped positions=[7,6,5,4,0,3,13,1]; 
+                    abca
+            ????????????a????    stamped positions=[8,7,6,5,4,0,3,13,1]; 
+                     abca
+            ?????????????????    stamped positions=[9,8,7,6,5,4,0,3,13,1];
+      5- if target becomes full of ? return res else it is impossible to reach the target so return []
+      
+      If it was asked to find minimum number of stamps, the problem would be difficult.
+
+ */
 var movesToStamp = function (stamp, target) {
 	const tagetChars = [...target],
 		lastPossibleIndex = target.length - stamp.length,
@@ -35,16 +68,16 @@ var movesToStamp = function (stamp, target) {
 			for (let i = index; i < index + stamp.length; i++) tagetChars[i] = "?";
 			placedPositions[index] = true;
 			res.unshift(index);
-		};
-	res = [];
-	let isMatchFound = true;
-	while (isMatchFound) {
+		},
+		res = [];
+
+	while (true) {
 		const nextStamp = getNextMove();
-		isMatchFound = nextStamp.found;
-		isMatchFound && stampPos(nextStamp.pos);
+		if (!nextStamp.found) break;
+		stampPos(nextStamp.pos);
 	}
 	return tagetChars.findIndex(el => el !== "?") === -1 ? res : [];
 };
-
+console.log(movesToStamp("abca", "aabcacaaaaaaaabca"));
 // "aabcacaaaaaaaabca"
 //  .xxxx........xxxx
